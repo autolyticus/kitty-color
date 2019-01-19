ca458913553cc0078763d3c080507115ceff5be5
reisub0 <reisub0@gmail.com>
2019-01-20 01:26:26 +0530

    Initial commit
    Signed-off-by: reisub0 <reisub0@gmail.com>

---
+++ a.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+CMD='sed '"'"'s|\# |\#: |'"'"' | sed '"'"'s|=||'"'"' | grep -v foreground_bold | grep -v '"'"'[color]'"'"''
+files="$(find -regex '.*.config')"
+
+find termite-colors -type d -print0 >dirs.txt
+sed -i 's/termite/kitty/g' dirs.txt
+xargs -0 mkdir -p < dirs.txt
+
+for i in $files; do
+    echo "$i"
+    cat "$i" | sed 's|\# |\#: |' | sed 's|=||' | grep -v foreground_bold > ${i//termite/kitty}
+done
